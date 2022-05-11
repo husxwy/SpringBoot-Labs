f6ef5c137558a9cc3981a187503520c726f5ecc9
husx <husxwy@163.com>
2022-05-11 12:20:18 +0800

    添加 测试

---
+++ hu-000-server/hu-000-Undertow/src/main/java/com/example/hu001httpclient4/Hu001Httpclient4Application.java
@@ -0,0 +1,13 @@
+package com.example.hu001httpclient4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hu001Httpclient4Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Hu001Httpclient4Application.class, args);
+  }
+
+}
---
+++ hu-000-server/hu-000-Undertow/src/test/java/com/example/hu001httpclient4/Hu001Httpclient4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hu001httpclient4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hu001Httpclient4ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
