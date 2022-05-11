7b191088e41b163e0c36a71fb8f2d80cffd2864e
husx <husxwy@163.com>
2022-05-11 11:58:03 +0800

    添加 测试

--- lab-03-kafka/lab-03-kafka-demo-concurrency/src/main/java/cn/iocoder/springboot/lab03/kafkademo/consumer/Demo06Consumer.java
+++ lab-03-kafka/lab-03-kafka-demo-concurrency/src/main/java/cn/iocoder/springboot/lab03/kafkademo/consumer/Demo06Consumer.java
@@ -6,16 +6,28 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * concurrency 大于分区个数时，也只有 分区个数线程消费
+ *   小于个数时，按照配置线程数消费
+ */
 @Component
 public class Demo06Consumer {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
+    Set a  = new HashSet();
+
     @KafkaListener(topics = Demo06Message.TOPIC,
             groupId = "demo06-consumer-group-" + Demo06Message.TOPIC,
-            concurrency = "2")
+            concurrency = "1")
     public void onMessage(Demo06Message message) {
         logger.info("[onMessage][线程编号:{} 消息内容：{}]", Thread.currentThread().getId(), message);
+
+        a.add(Thread.currentThread().getId());
+        logger.info("消费线程个数 {}, 线程Id {}",a.size(),a);
     }
 
 }
--- lab-03-kafka/lab-03-kafka-demo-concurrency/src/test/java/cn/iocoder/springboot/lab03/kafkademo/producer/Demo06ProducerTest.java
+++ lab-03-kafka/lab-03-kafka-demo-concurrency/src/test/java/cn/iocoder/springboot/lab03/kafkademo/producer/Demo06ProducerTest.java
@@ -24,8 +24,8 @@ public class Demo06ProducerTest {
 
     @Test
     public void testSyncSend() throws ExecutionException, InterruptedException {
-        for (int i = 0; i < 10; i++) {
-            int id = (int) (System.currentTimeMillis() / 1000);
+        for (int i = 0; i < 100; i++) {
+            int id = (int) (System.currentTimeMillis() );
             SendResult result = producer.syncSend(id);
 //        logger.info("[testSyncSend][发送编号：[{}] 发送结果：[{}]]", id, result);
         }
