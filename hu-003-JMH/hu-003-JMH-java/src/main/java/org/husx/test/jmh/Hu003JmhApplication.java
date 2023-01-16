16cec6dccdcc3bb5cb9bc8b19bb52038fb261c0b
sx hu <husxwy@163.com>
2023-01-16 17:43:39 +0800

    auto 20230115

---
+++ hu-003-JMH/hu-003-JMH-java/src/main/java/org/husx/test/jmh/Hu003JmhApplication.java
@@ -0,0 +1,13 @@
+package org.husx.test.jmh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hu003JmhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hu003JmhApplication.class, args);
+	}
+
+}
---
+++ hu-003-JMH/hu-003-JMH-java/src/test/java/org/husx/test/jmh/Hu003JmhApplicationTests.java
@@ -0,0 +1,13 @@
+package org.husx.test.jmh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hu003JmhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
