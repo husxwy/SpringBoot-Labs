b3b0aa1e3ebcc50ca0965ab5ad2081f1b7dabff1
sx hu <husxwy@163.com>
2023-01-17 13:58:54 +0800

    start init PostConstruct  CommandLineRunner

---
+++ hu-000-demo/ali-web/src/main/java/com/example/aliweb/AliWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.aliweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliWebApplication.class, args);
+    }
+
+}
